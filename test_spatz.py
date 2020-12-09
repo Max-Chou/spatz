cadbaf75343f138359812643d4f592ac60d57292
CHI-HUI CHOU <afai97202013@gmail.com>
2020-12-09 13:17:42 +0800

    test: add a test for duplicate routes

--- test_spatz.py
+++ test_spatz.py
@@ -12,3 +12,14 @@ def test_basic_route_adding(spatz):
     @spatz.route("/home")
     def home(req, resp):
         resp.text = "Hello"
+
+
+def test_route_overlap_throws_exception(spatz):
+    @spatz.route("/home")
+    def home(req, resp):
+        resp.text = "Hello"
+
+    with pytest.raises(AssertionError):    
+        @spatz.route("/home")
+        def home2(req, resp):
+            resp.text = "Hello"
