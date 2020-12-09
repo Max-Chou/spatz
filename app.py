18a7e8dcc539b6942365b781085b70a5bbb6ce87
CHI-HUI CHOU <afai97202013@gmail.com>
2020-12-09 10:32:48 +0800

    feat: add a function handling request

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+from spatz import Spatz
 
 
-def app(environ, start_response):
-    response_body = b"Hello, World!"
-    status = "200 OK"
-    start_response(status, headers=[])
-    return iter([response_body])
+app = Spatz()
+
+
+@app.route("/home")
+def home(request, response):
+    response.text = "Hello from the HOME page"
+
+
+@app.route("/about")
+def about(request, response):
+    response.text = "Hello from the ABOUT page"
--- spatz.py
+++ spatz.py
@@ -3,11 +3,45 @@ from webob import Request, Response
 
 class Spatz():
 
+    def __init__(self):
+
+        self.routes = {}
+
     def __call__(self, environ, start_response):
 
         request = Request(environ)
 
-        response = Response()
-        response.text = "Hello, World!"
-
+        response = self.handle_request(request)
+        
         return response(environ, start_response)
+
+
+    def route(self, path):
+        """Register the function to the given path
+
+        :param path: url path
+        :type path: string
+        """
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+
+        return wrapper
+
+
+    def handle_request(self, request):
+        """Handle requests and dispatch the requests to view functions
+
+        :param request: requests from clients
+        :type request: webob.Request
+        :return: responses from view functions
+        :rtype: webob.Response
+        """
+        response = Response()
+
+        # search for path and handler
+        for path, handler in self.routes.items():
+            if path == request.path:
+                handler(request, response)
+                return response
+        
